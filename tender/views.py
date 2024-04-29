4c89fae30fb6aa9b4f9b716e96b0c2034cb3c4c8
cozan5 <franmecoza@gmail.com>
2024-04-29 18:30:44 +0200

    search + categories

--- tender/views.py
+++ tender/views.py
@@ -5,49 +5,64 @@ from .models import Tender, Category
 from .forms import TenderForm
 
 # Create your views here.
+
+
 def tender_list(request):
     all_tenders = Tender.objects.all()
-    context={
+    context = {
         'Tenders': all_tenders
     }
-    return render(request,'tenders/tenders.html',context)
+    return render(request, 'tenders/tenders.html', context)
+
 
 @login_required
 def tender_details(request, tenderID):
-    tender=get_object_or_404(Tender, pk=tenderID)
-    context ={
-        'Tender':tender
+    tender = get_object_or_404(Tender, pk=tenderID)
+    context = {
+        'Tender': tender
     }
-    return render(request,'tenders/tender.html',context)
+    return render(request, 'tenders/tender.html', context)
+
 
 @login_required
 def tender_create(request):
-    context ={
-                'TenderCategories': Category.objects.all(),
-                'TenderForm': TenderForm()
-            }
-    if request.method=='POST':
+    context = {
+        'TenderCategories': Category.objects.all(),
+        'TenderForm': TenderForm()
+    }
+    if request.method == 'POST':
         tender_form = TenderForm(request.POST, request.FILES)
         if tender_form.is_valid():
             createdTender = tender_form.save(commit=False)
             createdTender.publisher = request.user
             createdTender.save()
-            
+
             messages.success(request, 'Tender created')
-            return render(request,'tenders/create.html',context)
+            return render(request, 'tenders/create.html', context)
         else:
             messages.error(request, 'Failed to create tender')
-            return render(request,'tenders/create.html',{
-             'TenderCategories': Category.objects.all(),
-             'TenderForm': tender_form
-        })
+            return render(request, 'tenders/create.html', {
+                'TenderCategories': Category.objects.all(),
+                'TenderForm': tender_form
+            })
     else:
-        return render(request,'tenders/create.html',context)
-    
+        return render(request, 'tenders/create.html', context)
+
+
 @login_required
 def published_tenders(request):
     owner = request.user
-    tendersByOwner = Tender.objects.filter(publisher = owner)
-    return render(request,'tenders/published-tenders.html',{
-             'TendersByOwner': tendersByOwner
-        })
+    tendersByOwner = Tender.objects.filter(publisher=owner)
+    return render(request, 'tenders/published-tenders.html', {
+        'TendersByOwner': tendersByOwner
+    })
+
+
+def search(request):
+    if request.method == "POST":
+        name = request.POST['searchy']
+        tender = Tender.objects.all().filter(title__contains=name)
+        context = {"tender": tender}
+        for ten in tender:
+            print(ten.title)
+        return render(request, "webpage/search.html", context)
--- webpage/views.py
+++ webpage/views.py
@@ -1,16 +1,15 @@
 from django.shortcuts import render
 from tender.models import Category, Tender
+from django.db.models import Count
 
 # Create your views here.
+
+
 def index(request):
-    category_name = request.GET.get('name',None)
-    tender_categories=Category.objects.all()
-    if category_name is not None:
-        tenderByCategoryLength = Tender.objects.filter(category = category_name).count()
-    else:
-        tenderByCategoryLength = 0
-    context ={
-        'TenderCategories': tender_categories,
-        'TendersCount': tenderByCategoryLength
+    categories = Category.objects.annotate(item_count=Count("id"))
+    # for obj in category_list:
+
+    context = {
+        'categories': categories,
     }
     return render(request, 'webpage/index.html', context)
