86d71a78233fe47dbc047cedbe52f17ced4b62c8
notfromaustralia <100213707+notfromaustralia@users.noreply.github.com>
2024-05-02 20:37:31 +0200

    added kyc_document

--- authentication/forms.py
+++ authentication/forms.py
@@ -4,15 +4,18 @@ from django.contrib.auth.forms import UserCreationForm
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Field, Submit
 
+
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(max_length=40,)
     company_name = forms.CharField(max_length=100,)
+    company_registration_no = forms.CharField(max_length=100,)
     mobile_number = forms.CharField(max_length=20,)
+    kyc_document = forms.FileField()
     address = forms.CharField(max_length=100,)
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'company_name', 'mobile_number', 'address', 'password1', 'password2')
+        fields = ('username', 'email', 'company_name', 'company_registration_no', 'mobile_number','kyc_document', 'address', 'password1', 'password2')
         help_texts = {
             'username': None,
             'password1': None,
--- authentication/models.py
+++ authentication/models.py
@@ -3,9 +3,17 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class SiteUser (models.Model):
+    status_choices = [
+        ('pending', 'Pending'),
+        ('rejected', 'Rejected'),
+        ('approved', 'Approved'),
+    ]
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     company_name = models.CharField(max_length=100)
+    company_registration_no = models.CharField(max_length=100, default='nil')
     mobile_number = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
+    kyc_document = models.FileField(upload_to='kyc_docs/', blank=True)
+    status = models.CharField(max_length=20, choices=status_choices, default='pending')
     def __str__(self):
         return self.user.username
